d64ccab14b76da06d75bca077e00489062c0c589
EmilioRivera0 <elcholula619@gmail.com>
2023-08-24 13:23:28 -0600

    Data Structures functions modification

--- data_structures.py
+++ data_structures.py
@@ -12,38 +12,26 @@ PATH_STACK = []
 def load_maze():
     """Loads the maze contained by maze.json, initialize metadata values and makes a serialization process 
     to change the 's' and 'i' values to 5 and 4 respectively.
-    Returns: MAZE, X_max, Y_max"""
+    Returns: MAZE, X_max, Y_max, start_point"""
     # maze extraction from json
     file = open("maze.json")
     temp_maze = load(file)["maze"]
-    # initialize metadata variables
+    # declare and initialize metadata variables
+    start_point = []
     X_max = len(temp_maze[0])
     Y_max = len(temp_maze)
     # serialization of loaded maze
-    # top and bottom rows serialization
-    for row_iterator in range(len(temp_maze[0])):
-        if temp_maze[0][row_iterator] == 's':
-            temp_maze[0][row_iterator] = 5
-        elif temp_maze[0][row_iterator] == 'i':
-            temp_maze[0][row_iterator] = 0
-    for row_iterator in range(len(temp_maze[0])):
-        if temp_maze[len(temp_maze)-1][row_iterator] == 's':
-            temp_maze[len(temp_maze)-1][row_iterator] = 5
-        elif temp_maze[len(temp_maze)-1][row_iterator] == 'i':
-            temp_maze[len(temp_maze)-1][row_iterator] = 0
-    # extreme left and right comuns serialization
-    for column_iterator in range(len(temp_maze)):
-        if temp_maze[column_iterator][0] == 's':
-            temp_maze[column_iterator][0] = 5
-        elif temp_maze[column_iterator][0] == 'i':
-            temp_maze[column_iterator][0] = 0
-    for column_iterator in range(len(temp_maze)):
-        if temp_maze[column_iterator][len(temp_maze[0])-1] == 's':
-            temp_maze[column_iterator][len(temp_maze[0])-1] = 5
-        elif temp_maze[column_iterator][len(temp_maze[0])-1] == 'i':
-            temp_maze[column_iterator][len(temp_maze[0])-1] = 0
-    print(temp_maze)
-    return temp_maze, X_max, Y_max
+    for row_iterator in range(len(temp_maze)):
+        for column_iterator in range(len(temp_maze[row_iterator])):
+            # s = exit
+            if temp_maze[row_iterator][column_iterator] == 's':
+                temp_maze[row_iterator][column_iterator] = 5
+            # i = starting point
+            elif temp_maze[row_iterator][column_iterator] == 'i':
+                temp_maze[row_iterator][column_iterator] = 0
+                # initialize the starting point
+                start_point = [row_iterator,column_iterator]
+    return temp_maze, X_max, Y_max, start_point
 
 def push_path_stack(coordinates):
     """Push (x,y) coordinates to the PATH_STACK"""
